dac287f2979d7e250c01844d10e8738525cf52b1
Darek <darek.dyrda@me.com>
2018-05-05 18:37:55 +0200

    Create working app boilerplate
    Signed-off-by: Darek <darek.dyrda@me.com>

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <React.Fragment>
+        "Hello World"
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { 
+            loader: 'css-loader', 
+            options: { importLoaders: 1 } 
+          },
+          'postcss-loader'
+        ],
+        include: __dirname + '/src'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
